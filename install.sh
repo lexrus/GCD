c15a48e04fa5b5b33615f3fbc9268b7510a3ec2d
lexrus <lexrus@gmail.com>
2016-05-28 15:26:17 +0800

    WIP.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DISTRIBUTION = $(lsb_release -is)
 RELEASE = $(lsb_release -cs)
 
 apt-get update
-apt-get install apt-transport-https ca-certificates curl
+apt-get install apt-transport-https ca-certificates python-pip
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
@@ -13,12 +13,10 @@ echo "deb https://apt.dockerproject.org/repo ${DISTRIBUTION,,}-$RELEASE main" >>
 apt-get update
 apt-get install docker-engine
 
+pip install docker-compose
+
 service docker start
 
 echo "Docker installed."
-
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-
-chmod +x /usr/local/bin/docker-compose
 
 docker-compose up -d -f ghost.yml
